fee853115d153231547faf20ebba1cea38eead3e
root <chak8x8@gmail.com>
2025-01-10 14:26:03 +0000

    13. Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -16,7 +16,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	/* Check if the current node has at least one child */
 	if (tree->left != NULL || tree->right != NULL)
 	{
-		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+		return (1
+			+ binary_tree_nodes(tree->left) 
+			+ binary_tree_nodes(tree->right));
 	}
 
 	/* Otherwise, just calculate for the subtrees */
