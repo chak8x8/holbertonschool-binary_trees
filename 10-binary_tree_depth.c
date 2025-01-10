5b5ae35c86839ea0eea2f4f15cd426d612a1ae4e
root <chak8x8@gmail.com>
2025-01-10 09:25:34 +0000

    10. Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,7 +10,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth;
 
-	if (tree == NULL || tree->parnt == NULL)
+	if (tree == NULL || tree->parent == NULL)
 	{
 		return (0);
 	}
